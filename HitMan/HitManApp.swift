4c725eca7ec0a6c312aaaa1a4e9638303e8b0a54
Tanya Alvarez-Salsedo <tanyaalvsal@tamu.edu>
2024-03-16 22:15:26 -0500

    Initial Commit

---
+++ HitMan/HitManApp.swift
@@ -0,0 +1,17 @@
+//
+//  HitManApp.swift
+//  HitMan
+//
+//  Created by Alvarez-Salsedo, Tanya on 3/16/24.
+//
+
+import SwiftUI
+
+@main
+struct HitManApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
